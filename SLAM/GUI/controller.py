e3773bdcf966239e7a26699004a07bde91570417
crosling1 <daeho.kim1988@gmail.com>
2022-10-03 16:00:18 +0200

    GUI update

--- SLAM/GUI/controller.py
+++ SLAM/GUI/controller.py
@@ -6,15 +6,9 @@ Created on Mon Sep 26 21:02:15 2022
 """
 
 import board
-import time
 from adafruit_motorkit import MotorKit
-import curses
 
 kit = MotorKit(i2c=board.I2C())
-screen = curses.initscr()
-curses.noecho()
-curses.cbreak()
-screen.keypad(True)
 PWMvor = 0.75    # 0 bis 1 entspricht 0-100% PWM beim Vorwärtsfahren
 korr = 0.8       # Korrekturfaktor
 PWMzur = 0.65    # entspricht dem Faktor der PWM beim Rückwärtsfahren
@@ -27,25 +21,25 @@ def vorwaerts():
     kit.motor4.throttle = korr * PWMvor
     print("start")
 
-def zurueck(self):
+def zurueck():
     kit.motor1.throttle = -PWMzur
     kit.motor2.throttle = -PWMzur
     kit.motor3.throttle = -PWMzur
     kit.motor4.throttle = -PWMzur
     
-def rechtsDrehen(self):
+def rechtsDrehen():
     kit.motor1.throttle = -drehen
     kit.motor2.throttle = drehen
     kit.motor3.throttle = -drehen
     kit.motor4.throttle = drehen
     
-def linksDrehen(self):
+def linksDrehen():
     kit.motor1.throttle = drehen
     kit.motor2.throttle = -drehen
     kit.motor3.throttle = drehen
     kit.motor4.throttle = -drehen
     
-def stop(self):
+def stop():
     kit.motor1.throttle = 0
     kit.motor2.throttle = 0
     kit.motor3.throttle = 0

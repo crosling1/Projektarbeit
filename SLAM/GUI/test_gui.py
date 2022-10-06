326509b6c6ccf39c4e06f6dde4d3730e152c3389
crosling1 <daeho.kim1988@gmail.com>
2022-10-06 22:31:10 +0200

    GUI test

--- SLAM/GUI/test_gui.py
+++ SLAM/GUI/test_gui.py
@@ -67,11 +67,11 @@ class MainGUI(Tk):
             self.rplidar.send_signal(signal.SIGINT)
     
     def forward(self):
-        if(self.start_button['text'] == "stop" and self.modeVar == 1):
+        if(self.modeVar == 1):
             controller.vorwaerts()
         
     def reverse(self):
-        if(self.start_button['text'] == "stop" and self.modeVar == 1):
+        if(self.start_button['text'] == "stop"):
             controller.zurueck()
         
     def left(self):

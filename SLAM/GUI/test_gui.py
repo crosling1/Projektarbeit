c1a2ea981d94365d31c19c4dcc4b05e243159eb1
crosling1 <daeho.kim1988@gmail.com>
2022-10-09 23:08:18 +0200

    ego update

--- SLAM/GUI/test_gui.py
+++ SLAM/GUI/test_gui.py
@@ -56,8 +56,6 @@ class MainGUI(Tk):
         self.button_right = tk.Button(self.keyboardframe, command = self.right, text = 'right')
         self.button_right.grid(column=2, row=1, sticky=tk.E, padx=5, pady=5)
         self.bind("<Key>",self.keyEvent)
-        if(self.start_button['text'] == "stop"):
-            
         self.mainloop()
         
     def scenario_run(self):
